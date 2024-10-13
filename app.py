96ff4b0644e9d1ba066b3766ca4da99c18a86169
krishnapandey12-12 <krishnapandey9987@gmail.com>
2024-10-13 16:37:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-import numpy as mp
+import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 from dash import dash, html, dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
-import numpy as np
-import pandas as pd
 
 external_stylesheet = [
     {
@@ -18,24 +16,35 @@ external_stylesheet = [
 
 patients = pd.read_csv('state_wise_daily.csv')
 total = patients.shape[0]
-active = patients[patients['Status']=='Confirmed'].shape[0]
-recovered = patients[patients['Status']=='Recovered'].shape[0]
-deaths = patients[patients['Status']=='Deceased'].shape[0]
+active = patients[patients['Status'] == 'Confirmed'].shape[0]
+recovered = patients[patients['Status'] == 'Recovered'].shape[0]
+deaths = patients[patients['Status'] == 'Deceased'].shape[0]
 
-options=[
-    {'label':'All', 'value':'All'},
-    {'label':'Hospitalized', 'value':'Hospitalized'},
-    {'label':'Recovered', 'value':'Recovered'},
-    {'label':'Deceased', 'value':'Deceased'},
+options = [
+    {'label': 'All', 'value': 'All'},
+    {'label': 'Hospitalized', 'value': 'Hospitalized'},
+    {'label': 'Recovered', 'value': 'Recovered'},
+    {'label': 'Deceased', 'value': 'Deceased'},
 ]
+
+options1 = [
+    {'label': 'All', 'value': 'All'},
+    {'label': 'Mask', 'value': 'Mask'},
+    {'label': 'Sanitizer', 'value': 'Sanitizer'},
+    {'label': 'Oxygen', 'value': 'Oxygen'},
+]
+options2 = [{'label': 'Red Zone', 'value': 'Red Zone'},
+            {'label': 'Blue Zone', 'value': 'Blue Zone'},
+            {'label': 'Green Zone', 'value': 'Green Zone'},
+            {'label': 'Orange Zone', 'value': 'Orange Zone'}
+            ]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheet)
 
-
 app.layout = html.Div([
-    html.H1('Corona Virus Pandamic',
+    html.H1('Corona Virus Pandemic',
             style={'color': ' #fff',
-                   'text-align': 'centre'}),
+                   'text-align': 'center'}),
     html.Div([
         html.Div([
             html.Div([
@@ -71,33 +80,74 @@ app.layout = html.Div([
         ], className='col-md-3')
     ], className='row'),
     html.Div([
+        html.Div([
             html.Div([
                 html.Div([
-                    html.Div([
-                    dcc.Dropdown(id= 'picker',options =options, value='All'),
+                    dcc.Dropdown(id='plot-graph', options=options1, value='All'),
+                    dcc.Graph(id='graph')
+                ], className='card-body')
+            ], className='card bg-info')
+        ], className='col-md-6'),
+
+        html.Div([
+            html.Div([
+                html.Div([
+                    dcc.Dropdown(id='my_dropdown', options=options2, value='Status', style={"width": "100%"}),
+                    dcc.Graph(id='the_graph')
+                ], className='card-body bg-success')
+            ], className='card')
+        ], className='col-md-6')
+    ], className='row'),
+    html.Div([
+        html.Div([
+            html.Div([
+                html.Div([
+                    dcc.Dropdown(id='picker', options=options, value='All'),
                     dcc.Graph(id='bar')
-                ],className='card-body')
-            ],className='card')
-        ],className='col-md-12')
+                ], className='card-body')
+            ], className='card')
+        ], className='col-md-12')
 
     ], className='row')
-],className='container')
+], className='container')
+
 
 @app.callback(Output('bar', 'figure'), [Input('picker', 'value')])
 def update_graph(type):
-
     if type == 'All':
-        return{'data': [go.Bar(x=patients['State'],y=patients['Total'])],
-               'layout': go.Layout(title='State Total Count',plot_bgcolor='orange')}
+        return {'data': [go.Bar(x=patients['State'], y=patients['Total'])],
+                'layout': go.Layout(title='State Total Count', plot_bgcolor='orange')}
     if type == 'Hospitalized':
-        return{'data': [go.Bar(x=patients['State'],y=patients['Hospitalized'])],
-               'layout': go.Layout(title='State Total Count',plot_bgcolor='orange')}
+        return {'data': [go.Bar(x=patients['State'], y=patients['Hospitalized'])],
+                'layout': go.Layout(title='State Total Count', plot_bgcolor='orange')}
     if type == 'Recovered':
-        return{'data': [go.Bar(x=patients['State'],y=patients['Recovered'])],
-               'layout': go.Layout(title='State Total Count',plot_bgcolor='orange')}
+        return {'data': [go.Bar(x=patients['State'], y=patients['Recovered'])],
+                'layout': go.Layout(title='State Total Count', plot_bgcolor='orange')}
     if type == 'Deceased':
-        return{'data': [go.Bar(x=patients['State'],y=patients['Deceased'])],
-               'layout': go.Layout(title='State Total Count',plot_bgcolor='orange')}
+        return {'data': [go.Bar(x=patients['State'], y=patients['Deceased'])],
+                'layout': go.Layout(title='State Total Count', plot_bgcolor='orange')}
+
+
+@app.callback(Output('graph', 'figure'), [Input('plot-graph', 'value')])
+def generate_graph(type):
+    if type == 'All':
+        return {'data': [go.Scatter(x=patients['Status'], y=patients['Total'], mode='lines')],
+                'layout': go.Layout(title="Commodities Total Count", plot_bgcolor='pink')}
+    if type == 'Mask':
+        return {'data': [go.Scatter(x=patients['Status'], y=patients['Mask'], mode='lines')],
+                'layout': go.Layout(title="Commodities Total Count", plot_bgcolor='pink')}
+    if type == 'Sanitizer':
+        return {'data': [go.Scatter(x=patients['Status'], y=patients['Sanitizer'], mode='lines')],
+                'layout': go.Layout(title="Commodities Total Count", plot_bgcolor='pink')}
+    if type == 'Oxygen':
+        return {'data': [go.Scatter(x=patients['Status'], y=patients['Oxygen'], mode='lines')],
+                'layout': go.Layout(title="Commodities Total Count", plot_bgcolor='pink')}
+
+
+@app.callback(Output('the_graph', 'figure'), [Input('my_dropdown', 'value')])
+def generate_graph(my_dropdown):
+    piechart = px.pie(data_frame=patients, names=my_dropdown, hole=0.3)
+    return piechart
 
 
 if __name__ == '__main__':
